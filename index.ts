1ef4cb89d9df25f2a59bcdf3ec8a8a4276b620ef
yamatatsu <the.kinnikuman@gmail.com>
2019-05-04 17:07:44 +0900

    static-site

--- index.ts
+++ index.ts
@@ -1,16 +1,47 @@
-import cdk = require("@aws-cdk/cdk");
-import s3 = require("@aws-cdk/aws-s3");
+import cdk = require('@aws-cdk/cdk')
+import cloudfront = require('@aws-cdk/aws-cloudfront')
+import s3 = require('@aws-cdk/aws-s3')
+import s3deploy = require('@aws-cdk/aws-s3-deployment')
 
 class MyStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string) {
-    super(parent, name);
+    super(parent, name)
 
-    new s3.Bucket(this, "MyBucket", {
-      bucketName: "bucket-tte-baketsu-no-koto-nandane"
-    });
+    const siteBucket = new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'bucket-tte-baketsu-no-koto-nandane',
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+    })
+
+    new s3deploy.BucketDeployment(this, 'MySiteDeployment', {
+      source: s3deploy.Source.asset('./dist'),
+      destinationBucket: siteBucket,
+    })
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      'MyWebDistribution',
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      },
+    )
+
+    new cdk.CfnOutput(this, 'DistributionId', {
+      value: distribution.distributionId,
+    })
+    new cdk.CfnOutput(this, 'DomainName', {
+      value: distribution.domainName,
+    })
   }
 }
 
-const app = new cdk.App();
+const app = new cdk.App()
 
-new MyStack(app, "CdkAample");
+new MyStack(app, 'CdkAample')
